b91ed25a6f7c655d6c33e99526e58681668fc6c6
wanghaichao <m18888106873@163.com>
2021-09-08 14:39:01 +0800

    修改分词器加载顺序

--- src/main/java/demo/spring/boot/demospringboot/util/WordUtil.java
+++ src/main/java/demo/spring/boot/demospringboot/util/WordUtil.java
@@ -2,6 +2,7 @@ package demo.spring.boot.demospringboot.util;
 
 import org.apdplat.word.WordSegmenter;
 import org.apdplat.word.segmentation.Word;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,14 +10,14 @@ import java.util.List;
 /**
  * 分词工具
  */
-//@Component
+@Component
 public class WordUtil {
 
     static {
         /**
          * 项目启动就调用(加载分词)
          */
-//        WordSegmenter.seg("加载分词init");
+        WordSegmenter.seg("加载分词init");
     }
 
     public static List<String> seg(String source) {
